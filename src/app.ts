462e1bd9d88998e643c1220e07cd67ae0035da72
rabby-hasan <rabby.65301@gmail.com>
2024-05-21 10:55:11 +0600

    configure eslint and prettier

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,8 @@
-import express from 'express'
-const app = express()
-
+import express from 'express';
+const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
+  res.send('Hello World Programmer!');
+});
 
 export default app;
