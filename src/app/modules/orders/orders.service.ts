6a4526ef0d7f83d3be20190cd1147572539bb530
rabby-hasan <rabby.65301@gmail.com>
2024-05-22 00:02:43 +0600

    create query search by email

--- src/app/modules/orders/orders.service.ts
+++ src/app/modules/orders/orders.service.ts
@@ -3,16 +3,24 @@ import { Order } from './orders.model';
 
 // create order and put in database
 const createOrderIntoDB = async (order: Orders) => {
-  const result = await Order.create(order);
-  return result;
+    const result = await Order.create(order);
+    return result;
 };
 //  get all order from database
-const getAllOrderIntoDB = async () => {
-  const result = await Order.find({});
-  return result;
+const getAllOrderIntoDB = async (query: object | null) => {
+    if (query) {
+        console.log("query")
+        const result = await Order.find(query);
+        return result;
+    } else {
+        console.log("data")
+        const result = await Order.find({});
+        return result;
+    }
+
 };
 
 export const orderService = {
-  createOrderIntoDB,
-  getAllOrderIntoDB,
+    createOrderIntoDB,
+    getAllOrderIntoDB,
 };
